205f62246108442f73294c8ceb818efa290431bb
Karan Anand <thekarananand@gmail.com>
2024-06-15 23:40:12 +0530

    upload-bunch-of-changes

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,9 +4,16 @@ from . import views
 
 urlpatterns = [
     path('', views.getRoutes, name='getRoutes' ),
-    path('ping/', views.getRoutes, name='getPing' ),
+    path('ping/', views.getRoutes,  name='getPing' ),
     path('input/', views.createPing, name='createPing' ),
 ]
+
+# urlpatterns += [
+#     path('/articles/',        views.articles,         name='articles'      ),
+#     path('/articles/new/',    views.articles_new,     name='articles_new'  ),
+#     path('/articles/id/',     views.articles_id,      name='articles_id'   ),
+#     path('/articles/id/md/',  views.articles_id_md,   name='articles_id_md'),
+# ]
 
 """
 Planned RestfulAPI Routes
@@ -14,6 +21,7 @@ Planned RestfulAPI Routes
 GET     /articles/
 POST    /articles/new/
 GET     /articles/id/
+GET     /articles/id/md/
 PUT     /articles/id/
 DELETE  /articles/id/
 """
--- backend/core/ConnectionConfig.py
+++ backend/core/ConnectionConfig.py
@@ -1,4 +1,14 @@
+debug_status = True
+
+allowed_host_list = [ '*' ]
+
+# allowed_host_list = [
+#     'localhost',
+#     'wikinetes-backend'
+# ]
+
 cors_origin_list = [
     'http://localhost:3000',
+    'http://localhost:8000',
     'http://wikinetes-frontend:3000',
 ]
