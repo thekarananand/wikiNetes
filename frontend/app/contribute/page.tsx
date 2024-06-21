bc43b93b0ef63f5f84cd5a79a2d394d9ecfd6b88
Karan Anand <thekarananand@gmail.com>
2024-06-21 10:21:04 +0530

    feat: add /contribute route

---
+++ frontend/app/contribute/page.tsx
@@ -0,0 +1,28 @@
+// Next Components
+import Link  from "next/link";
+
+// Styling
+import "./page.style.css";
+
+export default function Page() {
+    return (
+        <ul>
+            <li>
+                <Link href={ "/contribute/new/" }>
+                    Share Your Knowledge: Write a New Article
+                </Link>
+            </li>
+            <li>
+                <Link href={ "/https://github.com/thekarananand/wikiNetes/" }>
+                    Help Us Build: Contribute Code
+                </Link>
+            </li>
+            <li>
+                <Link href={ "/https://github.com/thekarananand/wikiNetes/" }>
+                    Report a Bug or Suggest a Feature:
+                    Raise an Issue on GitHub
+                </Link>
+            </li>
+        </ul>
+    );
+}
