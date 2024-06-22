31c9f7166cc3e52a7b8e71072d3a52ecae233ce4
Karan Anand <thekarananand@gmail.com>
2024-06-22 14:41:16 +0530

    feat: add /md/ route

---
+++ frontend/app/md/page.tsx
@@ -0,0 +1,10 @@
+// Styling
+import "./page.style.css";
+
+export default function Page() {
+    return (
+        <main className="main">
+            <h1>We Support Markdown</h1>
+        </main>
+    );
+}
